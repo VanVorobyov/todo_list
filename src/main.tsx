8b4eefedae96cf1bc4f3664fab5adbeaa4231071
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-10-01 23:13:21 +0300

    start: add styles and the structure of the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './views/App';
+
+import './views/vendor/normalize.scss';
+import './views/styles/common.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
